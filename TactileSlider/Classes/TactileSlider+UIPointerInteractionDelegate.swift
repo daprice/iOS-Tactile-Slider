e0949b8712f1189ab14873549c469214341e0e62
Dale Price <daprice@mac.com>
2021-08-18 17:41:56 -0500

    Revert "Add `@available` declarations to each method in TactileSlider+UIPointerInteractionDelegate.swift"
    This reverts commit 43cfd9cc5c78da07be156e66fad94438a6352c87.

--- TactileSlider/Classes/TactileSlider+UIPointerInteractionDelegate.swift
+++ TactileSlider/Classes/TactileSlider+UIPointerInteractionDelegate.swift
@@ -10,20 +10,13 @@ import UIKit
 
 @available(iOS 13.4, *)
 @available(tvOS, unavailable)
-@available(macOS, unavailable)
 extension TactileSlider: UIPointerInteractionDelegate {
 	
-	@available(iOS 13.4, *)
-	@available(tvOS, unavailable)
-	@available(macOS, unavailable)
 	internal func setUpPointerInteraction() {
 		let pointerInteraction = UIPointerInteraction(delegate: self)
 		self.addInteraction(pointerInteraction)
 	}
 	
-	@available(iOS 13.4, *)
-	@available(tvOS, unavailable)
-	@available(macOS, unavailable)
 	public func targetedPreview(for interaction: UIPointerInteraction) -> UITargetedPreview? {
 		if let interactionView = interaction.view {
 			let previewParameters = UIPreviewParameters()
@@ -37,25 +30,16 @@ extension TactileSlider: UIPointerInteractionDelegate {
 	}
 	
 	/// override this to set a custom pointer interaction style
-	@available(iOS 13.4, *)
-	@available(tvOS, unavailable)
-	@available(macOS, unavailable)
 	open func pointerStyle(with targetedPreview: UITargetedPreview) -> UIPointerStyle {
 		return UIPointerStyle(
 			effect: UIPointerEffect.hover(targetedPreview, preferredTintMode: UIPointerEffect.TintMode.overlay, prefersShadow: false, prefersScaledContent: true)
 		)
 	}
 	
-	@available(iOS 13.4, *)
-	@available(tvOS, unavailable)
-	@available(macOS, unavailable)
 	open func pointerInteraction(_ interaction: UIPointerInteraction, regionFor request: UIPointerRegionRequest, defaultRegion: UIPointerRegion) -> UIPointerRegion? {
 		return UIPointerRegion(rect: self.bounds)
 	}
 	
-	@available(iOS 13.4, *)
-	@available(tvOS, unavailable)
-	@available(macOS, unavailable)
 	open func pointerInteraction(_ interaction: UIPointerInteraction, styleFor region: UIPointerRegion) -> UIPointerStyle? {
 		var style: UIPointerStyle? = nil
 		
